73ac48ec26287c67e2001ff98679ddc97f90a0e1
John Henry Thompson <jht9629@nyu.edu>
2024-04-26 05:26:39 -0400

    370

---
+++ videoKit/370/core/liveMedia_attach.js
@@ -0,0 +1,103 @@
+import { a_ } from '../let/a_state.js?v=370';
+import { create_mediaDiv, remove_mediaDiv } from '../core/create_mediaDiv.js?v=370';
+import { ui_refresh } from '../core-ui/ui_patch_bar.js?v=370';
+import { ui_chat_receive } from '../core-ui/ui_chat.js?v=370';
+
+// let a_livem;
+
+export function liveMedia_attach(mediaDiv) {
+  // console.log('liveMedia_attach mediaDiv=', mediaDiv);
+  let type;
+  let stream;
+  let mediaDevice = mediaDiv.mediaDevice;
+  if (mediaDevice) {
+    stream = mediaDevice.stream;
+    if (!stream) {
+      console.log('liveMedia_attach NO stream mediaDiv=', mediaDiv);
+      return;
+    }
+    type = 'CAPTURE';
+  } else if (!a_.ui.canvas_data_chk) {
+    // no mediaDevice --> canvas
+    stream = a_.my_canvas;
+    type = 'CANVAS';
+  } else {
+    // Data only - don't stream out our canvas
+    stream = null;
+    type = 'DATA';
+  }
+  let livem = mediaDiv.livem;
+  if (livem) {
+    console.log('liveMedia_attach livem', livem);
+    return;
+  }
+  // console.log('liveMedia_attach this=', this);
+  // console.log('liveMedia_attach type=' + type + ' a_.ui.room_name=' + a_.ui.room_name);
+  // this is nulll in modules
+  let nthis = this || window;
+  console.log('liveMedia_attach this', this, 'nthis', nthis);
+  let room_name = a_.videoKit.room_name_prefix + a_.ui.room_name;
+  console.log('liveMedia_attach room_name', room_name);
+  livem = new p5LiveMedia(nthis, type, stream, room_name);
+  if (!a_.livem) {
+    livem.on('stream', gotStream);
+    livem.on('data', gotData);
+    livem.on('disconnect', gotDisconnect);
+    livem.on('connect', gotConnect);
+    a_.livem = livem;
+    // console.log('liveMedia_attach SET a_.livem', a_.livem);
+  }
+  mediaDiv.livem = livem;
+}
+
+export function liveMedia_detach(mediaDiv) {
+  console.log('liveMedia_detach mediaDiv=', mediaDiv);
+  if (!mediaDiv) return;
+  mediaDiv.livem = null;
+}
+
+// For debugging
+let otherVideo;
+
+// We got a new stream!
+function gotStream(capture, id) {
+  console.log('gotStream id', id);
+  console.log('gotStream width', capture.width, 'height', capture.height);
+  // This is just like a video/stream from createCapture(VIDEO)
+  otherVideo = capture;
+  //otherVideo.id and id are the same and unique identifiers
+  capture.elt.muted = true;
+  let stream = capture.elt.srcObject;
+  let deviceId = id;
+  let mediaDevice = { deviceId, capture, stream };
+  create_mediaDiv(mediaDevice, { live: 1 });
+  ui_refresh();
+  // livem_send('Hello');
+}
+
+// loadedmetadata
+
+function gotData(theData, id) {
+  console.log('gotData theData', theData, 'id', id);
+  ui_chat_receive(theData, id);
+}
+
+function gotDisconnect(id) {
+  console.log('gotDisconnect id', id);
+  ui_chat_receive('', id);
+  remove_mediaDiv(id);
+}
+
+function gotConnect(id) {
+  console.log('gotConnect id', id);
+}
+
+export function livem_send(text) {
+  console.log('livem_send text', text);
+  if (!a_.livem) return;
+  let name = a_.ui.chat_name;
+  let obj = { name, text };
+  a_.livem.send(JSON.stringify(obj));
+}
+
+// https://github.com/vanevery/p5LiveMedia
---
+++ videoKit/370/effects/cset/eff_loop.js
@@ -0,0 +1,228 @@
+import { effectMeta_find, factory_prop_inits } from '../../core/effectMeta.js?v=370';
+import { image_scaled_pad } from '../../util/image.js?v=370';
+import { PeriodTimer } from '../../util/PeriodTimer.js?v=370';
+import { patch_index1 } from '../../core-ui/ui_patch_eff.js?v=370';
+
+export default class eff_loop {
+  static meta_props = {
+    period: [1, 0, 0.5, 1, 2, 3, 4, 5, 6, 8, 10, 15, 20, 30, 60],
+    // phase_check: [0, 1],
+    next: {
+      button: (ent, aPatch) => {
+        ent.next_action(aPatch);
+      },
+    },
+    prev: {
+      button: (ent, aPatch) => {
+        ent.prev_action(aPatch);
+      },
+    },
+    step_patch: [0, 2, 3],
+    _freeze_patch: [0, 1],
+    freeze_screen: [0, 1],
+    _effects: {
+      style: 'width:80%',
+      // text_input: 'sketchy,slant_scan,slit_scan',
+      text_input: 'bright,delaunay,grid,maze,sketchy,slant_scan,slit_scan',
+    },
+  };
+  static eff_names = [
+    'bright',
+    'delaunay',
+    'grid',
+    'bright',
+    'maze',
+    'sketchy',
+    'slant_scan',
+    'slit_scan',
+  ];
+  constructor(props) {
+    Object.assign(this, props);
+    this.basic_props = Object.assign({}, props);
+    this.init();
+  }
+  prepareOutput() {
+    this.advance_check();
+    if (this.advancePending) {
+      this.patch_step();
+      this.trigger_step();
+      this.advancePending = 0;
+    }
+    let other = this.prepare_input(this.eff_inst);
+    if (other) {
+      if (this.freeze_patch && this.wasFrozen && this.got_freeze) {
+        // console.log('eff_loop  ', this.got_freeze);
+        this.draw_freeze();
+      } else {
+        other.prepareOutput();
+        this.output = other.output;
+      }
+    }
+    this.period_timer.check(() => {
+      this.iperiod++;
+      if (this.has_phase_check) {
+        // Waiting for other.completed_phase
+      } else {
+        this.possibleAdvance();
+      }
+    });
+    if (0) {
+      let src = patch_index1(1);
+      if (src) {
+        src.draw = 'mesh';
+      }
+    }
+  }
+  possibleAdvance() {
+    if (this.freeze_patch && this.wasFrozen) {
+      return;
+    }
+    this.advancePending = 1;
+  }
+  prepare_input(other) {
+    if (other) {
+      other.input = this.input;
+    }
+    return other;
+  }
+  init() {
+    if (!this.effects) this.effects = 'show';
+    this.eff_names = this.effects.split(',');
+    this.index = 0;
+    this.eff_inst = null;
+    this.eff_inst_arr = [];
+    this.next_eff();
+    this.period_timer = new PeriodTimer(this.period);
+    this.iperiod = 0;
+    this.img_freeze = createImage(this.input.width, this.input.height);
+  }
+  deinit() {
+    for (let eff of this.eff_inst_arr) {
+      this.videoKit.deinitEffect(eff);
+    }
+  }
+  trigger_step() {
+    if (!this.step_patch) return;
+    let src = patch_index1(this.step_patch);
+    // console.log(  'trigger_step src', src, 'src.patch_stepper', src.patch_stepper );
+    if (src && src.patch_stepper) {
+      src.patch_stepper();
+    }
+  }
+  draw_freeze() {
+    image_scaled_pad(this.img_freeze, this.eff_spec.urect);
+  }
+  advance_check() {
+    if (this.freeze_patch) {
+      let src = patch_index1(this.freeze_patch);
+      if (src) {
+        // console.log('src.frozen', src.frozen);
+        // if (!this.wasFrozen && src.frozen) {
+        //   this.advancePending = 1;
+        // }
+        this.wasFrozen = src.frozen;
+      }
+    }
+    if (this.has_completed_phase) {
+      let other = this.eff_inst;
+      if (other.completed_phase()) {
+        // this.advancePending = 1;
+        this.possibleAdvance();
+      }
+    }
+  }
+  patch_step() {
+    // console.log('eff_loop step');
+    this.next_eff();
+  }
+  next_action(aPatch) {
+    this.next_eff();
+  }
+  prev_action(aPatch) {
+    console.log('eff_loop c index', this.index);
+    this.index = this.index - 2;
+    let eff_names = this.eff_names;
+    this.index = (eff_names.length + this.index) % eff_names.length;
+    console.log('eff_loop d index', this.index);
+    this.next_eff();
+  }
+  next_eff() {
+    // console.log('eff_loop a index', this.index);
+    let eff_names = this.eff_names;
+    let label = eff_names[this.index];
+    this.index = (this.index + 1) % eff_names.length;
+    let effMeta = effectMeta_find(label);
+    // console.log('next_eff effMeta', effMeta);
+    if (effMeta) {
+      console.log('next_eff effMeta', effMeta.label);
+      // let iprops = this.eff_prop_inits(effMeta.factory.meta_props);
+      // let inits = this.eff_prop_inits(effMeta.factory.meta_props);
+      let inits = factory_prop_inits(effMeta.factory, this.basic_props);
+      // Set input on inits for eff_inst.init
+      this.prepare_input(inits);
+      let eff_inst = this.eff_inst_arr[this.index];
+      if (eff_inst) {
+        this.videoKit.deinitEffect(eff_inst);
+      }
+      eff_inst = new effMeta.factory(inits);
+      this.eff_inst_arr[this.index] = eff_inst;
+      // } else {
+      //   // console.log('next_eff init eff_inst', eff_inst);
+      //   eff_inst.init();
+      // }
+      this.eff_inst = eff_inst;
+      this.has_completed_phase = this.eff_inst.completed_phase;
+    }
+  }
+  // eff_prop_inits(dict) {
+  //   let inits = Object.assign({}, this.basic_props);
+  //   for (let prop in dict) {
+  //     // eg. items = factor: [10, 50, 100 ... ]
+  //     let items = dict[prop];
+  //     if (prop.substring(0, 1) === '_') {
+  //       prop = prop.substring(1);
+  //     }
+  //     if (Array.isArray(items)) {
+  //       // eg. items = factor: [10, 50, 100 ... ]
+  //       inits[prop] = items[0];
+  //     } else {
+  //       // eg: _next: { button: next_action }
+  //     }
+  //   }
+  //   return inits;
+  // }
+}
+
+// static eff_names = [
+//   'show',
+//   'bright',
+//   'show',
+//   'delaunay',
+//   'show',
+//   'grid',
+//   'show',
+//   'bright',
+//   'show',
+//   'maze',
+//   'show',
+//   'sketchy',
+//   'show',
+//   'slant_scan',
+//   'show',
+//   'slit_scan',
+// ];
+// static eff_names = ['show', 'sketchy'];
+// show will trigger patch_stepper in face_mesh
+
+// static eff_namesXX = [
+//   'show',
+//   'delaunay',
+//   'sketchy',
+//   'maze',
+//   'grid',
+//   // 'bright',
+//   // 'diff',
+//   // 'triangle',
+//   // 'slit_scan',
+//   // 'slant_scan',
+// ];
